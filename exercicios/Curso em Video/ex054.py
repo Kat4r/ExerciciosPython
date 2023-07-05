ad618ca7a7d6914d34b33d89739ce4586a2d9f05
Kat4r <viniciuspauloom@hotmail.com>
2023-07-05 14:26:46 -0300

    ex54 fix

--- exercicios/Curso em Video/ex054.py
+++ exercicios/Curso em Video/ex054.py
@@ -1,6 +1,6 @@
-import time as penis
-anoatual = penis.localtime()
-anoatual = int(penis.strftime('%Y', anoatual))
+import time
+anoatual = time.localtime()
+anoatual = int(time.strftime('%Y', anoatual))
 idademaior = idademenor = nascimento = frase = 0
 for frase in range(0,6):
     nascimento = int(input("digite a {}º idade: ".format(frase+1)))
@@ -13,4 +13,3 @@ for frase in range(0,6):
         idademenor = idademenor + 1
 print('Há \033[1:33m{}\033[m pessoas de \033[1:32mmaioridade\033[m nessa lista'.format(idademaior))
 print('Há \033[1:33m{}\033[m pessoas de \033[1:31mmenoridade\033[m nessa lista'.format(idademenor))
-
