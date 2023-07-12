907feee1993ac43bf0314e4fba0d53710efc906d
Kat4r <viniciuspauloom@hotmail.com>
2023-07-12 18:57:32 -0300

    np exercises

---
+++ exercicios/Data Science/Numpy/ex001.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+vetorUnidimensional = np.array([1,2,3])
+
+vetorBidimensional = np.array([[1,2,3],[4,5,6]])
+
+vetorTridimensional = np.array([[[1,2,3],[4,5,6],[7,8,9]]])
+
+
+
+print(vetorTridimensional.ndim)
+print(vetorTridimensional.shape)
+print(vetorTridimensional.size)
