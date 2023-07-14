caf6addaa99e09a49c9aa4c00ce5aa1d71240055
Kat4r <viniciuspauloom@hotmail.com>
2023-07-14 18:25:31 -0300

    update

--- exercicios/Data Science/Numpy/ex001.py
+++ exercicios/Data Science/Numpy/ex001.py
@@ -6,8 +6,12 @@ vetorBidimensional = np.array([[1,2,3],[4,5,6]])
 
 vetorTridimensional = np.array([[[1,2,3],[4,5,6],[7,8,9]]])
 
-
+vetorRange = np.arange(1,20,3)
 
 print(vetorTridimensional.ndim)
 print(vetorTridimensional.shape)
 print(vetorTridimensional.size)
+
+vetorRange[0] = 5
+
+print(vetorRange)
--- exercicios/Data Science/Pandas/ex001.py
+++ exercicios/Data Science/Pandas/ex001.py
@@ -1,6 +1,11 @@
 import pandas as pd
+import numpy as np
 
-d = {'col1': [1, 2], 'col2': [3, 4]}
-df = pd.DataFrame(data=d)
+lista1 = [1,2,3]
+lista2 = ['a','b','c']
 
+listaUnida = pd.Series(lista2, index=lista1) #cria uma serie, o index é totalmente opcional
+
+dic = {'Impares':np.arange(1,21,2), 'Pares':np.arange(2,22,2), 'Sequencial':np.arange(1,11)} #criando uma estrutura de dataframe
+df = pd.DataFrame(dic)
 print(df)
